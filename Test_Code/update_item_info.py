91cc783a1c4ea27ac9f47246ccababca439a5c02
kvlb23 <kvlb23@ndw1800.isis.cclrc.ac.uk>
2024-09-03 14:55:52 +0100

    Use <> instead of [] for substitution pattern

--- Test_Code/update_item_info.py
+++ Test_Code/update_item_info.py
@@ -13,22 +13,22 @@ class IssueToUpdate:
         set_proj_mutation = gql_queries.open_graph_ql_query_file("setProject.txt")
 
         result = gql_queries.run_query(
-            set_proj_mutation.replace("[ISSUE_ID]", self.issue_id).replace("[PROJ_ID]", self.project_to_use.project_id))
+            set_proj_mutation.replace("<ISSUE_ID>", self.issue_id).replace("<PROJ_ID>", self.project_to_use.project_id))
         self.item_id = result["data"]["addProjectV2ItemById"]["item"]["id"]
 
     def set_sprint(self, sprint_to_use):
         set_sprint = gql_queries.open_graph_ql_query_file("UpdateSprintForItemInProject.txt")
-        gql_queries.run_query(set_sprint.replace("[ITEM_ID]", self.item_id)
-                                    .replace("[SPRINT_FIELD_ID]", self.project_to_use.sprint_field_id)
-                                    .replace("[SPRINT_ID]", sprint_to_use)
-                                    .replace("[PROJ_ID]", self.project_to_use.project_id))
+        gql_queries.run_query(set_sprint.replace("<ITEM_ID>", self.item_id)
+                                    .replace("<SPRINT_FIELD_ID>", self.project_to_use.sprint_field_id)
+                                    .replace("<SPRINT_ID>", sprint_to_use)
+                                    .replace("<PROJ_ID>", self.project_to_use.project_id))
 
     def set_status(self, status_to_use):
         set_sprint = gql_queries.open_graph_ql_query_file("UpdateStatusForItemInProject.txt")
-        gql_queries.run_query(set_sprint.replace("[ITEM_ID]", self.item_id)
-                              .replace("[STATUS_FIELD_ID]", self.project_to_use.status_field_id)
-                              .replace("[STATUS_ID]", status_to_use)
-                              .replace("[PROJ_ID]", self.project_to_use.project_id))
+        gql_queries.run_query(set_sprint.replace("<ITEM_ID>", self.item_id)
+                              .replace("<STATUS_FIELD_ID>", self.project_to_use.status_field_id)
+                              .replace("<STATUS_ID>", status_to_use)
+                              .replace("<PROJ_ID>", self.project_to_use.project_id))
 
     def place_in_next_sprint(self):
         self.set_sprint(self.project_to_use.sprint_ids[self.project_to_use.next_sprint])
