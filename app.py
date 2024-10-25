65d05257a0f12bc2a13dab68b7c8c776d154cbc6
kvlb23 <kvlb23@ndw1800.isis.cclrc.ac.uk>
2024-10-25 15:29:13 +0100

    Verify webhook has come from GitHub

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import asyncio
 import tornado
 from github_interactions import get_project_info, update_item_info
 import os
+import hashlib
+import hmac
+import configparser
 
 # Initialise the classes that will be needed for the overall app
 current_project = get_project_info.ProjectInfo()
-
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), "config_info","config.ini"))
+secret = config["GITHUB.INTERACTION"]["webhook_secret"]
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -17,8 +22,37 @@ class TestHandler(tornado.web.RequestHandler):
         self.write("This is my test")
 
 
+class WebhookError(Exception):
+    def __init__(self, status_code, detail):
+        self.status_code = status_code
+        self.detail = detail
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise WebhookError(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise WebhookError(status_code=403, detail="Request signatures didn't match!")
+
+
 class WebhookHandler(tornado.web.RequestHandler):
     def post(self):
+        try:
+            verify_signature(self.request.body,secret,self.request.headers["X-Hub-Signature-256"])
+        except WebhookError as e:
+            print("Found a signature issue: " + e.detail)
+            self.set_status(e.status_code)
+            return
         request = tornado.escape.json_decode(self.request.body)
         # This is assuming a post from GitHub, other sources will not do much
         match request["action"]:
--- webserver_forJSONBourne_and_git_automation.py
+++ webserver_forJSONBourne_and_git_automation.py
@@ -6,6 +6,9 @@ from logging.handlers import TimedRotatingFileHandler
 import tornado.ioloop
 import tornado.web
 import asyncio
+import hashlib
+import hmac
+import configparser
 
 from external_webpage.request_handler_utils import get_detailed_state_of_specific_instrument, \
     get_summary_details_of_all_instruments, get_instrument_and_callback
@@ -16,6 +19,9 @@ from external_webpage.instrument_scapper import scraped_data, scraped_data_lock
 from github_interactions import get_project_info, update_item_info
 # Initialise the classes that will be needed for the overall automation work
 current_project = get_project_info.ProjectInfo()
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), "config_info","config.ini"))
+secret = config["GITHUB.INTERACTION"]["webhook_secret"]
 
 logger = logging.getLogger('JSON_bourne')
 log_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log', 'JSON_bourne.log')
@@ -78,9 +84,37 @@ class MyHandler(tornado.web.RequestHandler):
          for every client request. Remove this to re-enable """
         return
 
+class WebhookError(Exception):
+    def __init__(self, status_code, detail):
+        self.status_code = status_code
+        self.detail = detail
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise WebhookError(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise WebhookError(status_code=403, detail="Request signatures didn't match!")
+
 # Handlers anf functions for automation
 class WebhookHandler(tornado.web.RequestHandler):
     def post(self):
+        try:
+            verify_signature(self.request.body,secret,self.request.headers["X-Hub-Signature-256"])
+        except WebhookError as e:
+            print("Found a signature issue: " + e.detail)
+            self.set_status(e.status_code)
+            return
         request = tornado.escape.json_decode(self.request.body)
         # This is assuming a post from GitHub, other sources will not do much
         match request["action"]:
