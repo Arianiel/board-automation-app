0067e9b310b086781262665a0a5da280001019ea
kvlb23 <kvlb23@ndw1800.isis.cclrc.ac.uk>
2024-11-26 11:59:00 +0000

    Add the snapshot to the JSON Bourne version

--- webserver_forJSONBourne_and_git_automation.py
+++ webserver_forJSONBourne_and_git_automation.py
@@ -274,9 +274,22 @@ class AutomationHandler(tornado.web.RequestHandler):
 
 class ColumnFrequencyHandler(tornado.web.RequestHandler):
     def get(self):
+        snapshot = current_project.get_cards_snapshot()
         self.render(os.path.join(os.path.dirname(__file__), "pi_and_sprint_actions", "column_count_and_points.html"),
-                    current_sprint=current_project.current_sprint, next_sprint=current_project.next_sprint,
-                    misc_message=current_project.html_message)
+                    misc_message=current_project.html_message,
+                    ready_count=snapshot["ready"]["count"],
+                    ready_points=snapshot["ready"]["points"],
+                    rework_count=snapshot["rework"]["count"],
+                    rework_points=snapshot["rework"]["points"],
+                    in_progress_count=snapshot["in_progress"]["count"],
+                    in_progress_points=snapshot["in_progress"]["points"],
+                    impeded_count=snapshot["impeded"]["count"],
+                    impeded_points=snapshot["impeded"]["points"],
+                    review_count=snapshot["review"]["count"],
+                    review_points=snapshot["review"]["points"],
+                    done_count=snapshot["done"]["count"],
+                    done_points=snapshot["done"]["points"]
+                    )
 
 
 if __name__ == '__main__':
