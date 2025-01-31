66c8966f030f3cbb8703ac18699dc5b04660ba6a
kvlb23 <kvlb23@ndw1800.isis.cclrc.ac.uk>
2025-01-31 20:34:14 +0000

    #26 - test cards by status and list of issues in sprint

--- Tests/test_ql_interactions.py
+++ Tests/test_ql_interactions.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from unittest import TestCase
 import graph_ql_interactions.graph_ql_functions as ql
 import graph_ql_interactions.repo_interactions as ri
@@ -108,16 +109,48 @@ class TestCardInteractions(TestCase):
                                         expected_snapshot=expected_snapshot, sprint_name=sprint_name))
         self.assertDictEqual(ci.get_planning_snapshot("org_name", "0", sprint_name),
                              expected_snapshot)
-    
-    def test_get_number_of_cards_by_status(self):
-        # TODO
-        # Figure out how to test this!
-        pass
-    
-    def test_get_card_issue_ids_in_sprint(self):
-        # TODO
-        # Figure out how to test this!
-        pass
+
+    @requests_mock.mock()
+    def test_get_number_of_cards_by_status(self, m):
+        expected_snapshot = {
+            "ready": {"count": 3, "points": 3},
+            "rework": {"count": 1, "points": 1},
+            "in_progress": {"count": 2, "points": 2},
+            "impeded": {"count": 1, "points": 1},
+            "review": {"count": 3, "points": 3},
+            "done": {"count": 4, "points": 4},
+        }
+        alternative = []
+        for entry in expected_snapshot:
+            for value in range(0, expected_snapshot[entry]["count"]):
+                alternative.append(snapshot_name_to_status_lookup(entry))
+        sprint_name = "sprint"
+        m.post(url, text=build_response(QlCommand.findCardInfo, card_type="points_snapshot",
+                                        expected_snapshot=expected_snapshot, sprint_name=sprint_name))
+        self.assertCountEqual(ci.get_number_of_cards_by_status("org_name", "0", sprint_name),
+                             Counter(alternative))
+
+    @requests_mock.mock()
+    def test_get_card_issue_ids_in_sprint(self, m):
+        expected_snapshot = {
+            "ready": [{"number": 1, "repo": "repo_1"}, {"number": 2, "repo": "repo_1"}, {"number": 1, "repo": "repo_2"},
+                      {"number": 2, "repo": "repo_2"}],
+            "rework": [{"number": 1, "repo": "repo_1"}],
+            "in_progress": [{"number": 3, "repo": "repo_1"}, {"number": 4, "repo": "repo_1"}],
+            "impeded": [{"number": 3, "repo": "repo_2"}],
+            "review": [{"number": 4, "repo": "repo_2"}, {"number": 5, "repo": "repo_1"}],
+            "done": [{"number": 5, "repo": "repo_2"}, {"number": 6, "repo": "repo_3"}],
+        }
+        sprint_name = "sprint"
+        expected_issues = []
+        for entry in expected_snapshot:
+            if entry == "rework":
+                continue
+            for item in expected_snapshot[entry]:
+                expected_issues.append(f"issue_{item['number']}")
+        m.post(url, text=build_response(QlCommand.findCardInfo, card_type="card_list_snapshot",
+                                        expected_snapshot=expected_snapshot, sprint_name=sprint_name))
+        self.assertEqual(ci.get_card_issue_ids_in_sprint("org_name", "0", sprint_name), expected_issues)
     
     def test_add_label(self):
         # TODO
